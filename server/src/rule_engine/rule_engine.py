e0be3aa065ecb3bf15ddfeb156ea7fc2232f455c
Anmol Takiar <34587426+atakiar@users.noreply.github.com>
2020-11-14 15:52:08 -0500

    chore: format

--- server/src/rule_engine/rule_engine.py
+++ server/src/rule_engine/rule_engine.py
@@ -13,17 +13,19 @@ class RuleEngine:
     def __init__(self):
         # TODO: Dict of something
         self.symbols = {}
-        self.nums = {"one": 1,
-                     "two": 2,
-                     "three": 3,
-                     "four": 4,
-                     "five": 5,
-                     "six": 6,
-                     "seven": 7,
-                     "eight": 8,
-                     "nine": 9,
-                     "ten": 10
-                     }
+        self.nums = {
+            "zero": 0,
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+            "ten": 10,
+        }
 
     @classmethod
     def find_next(self, tokens, *containers):
@@ -62,7 +64,7 @@ class RuleEngine:
 
     @classmethod
     def consume_many(self, tokens, number):
-        return tokens[1 + number:]
+        return tokens[1 + number :]
 
     def parse(self, text: str):
         tokens = text.strip().split(" ")
@@ -86,26 +88,28 @@ class RuleEngine:
         else:
             print("NOT IMPLEMENTED EXCEPTION")
 
-    def _build_func_definition(name: str, params: List[str]) -> str:
+    def _build_func_definition(self, name: str, params: List[str]) -> str:
         paramstr = ", ".join(params)
         return f"def {name}({paramstr}):\n"
 
     def parse_function(self, tokens: List[str]) -> List[str]:
         arg_i, _ = self.find_next_specific(tokens, SecondaryKeywords.ARGUMENT)
         and_i, _ = self.find_next_specific(tokens, SecondaryKeywords.AND)
-        num_args = self.nums[tokens[arg_i-1]]
+        num_args = self.nums[tokens[arg_i - 1]]
         params = ["" for i in range(num_args)]
-        func_name = "_".join(tokens[1:arg_i-2])
+        func_name = "_".join(tokens[1 : arg_i - 2])
         if and_i != -1:
             params[-1] = " ".join(tokens[and_i:])
             tokens = tokens[:and_i]
             num_args = num_args - 1
-        tokens = tokens[arg_i + 1:]
-        for j in range(num_args-1):
+        tokens = tokens[arg_i + 1 :]
+        for j in range(num_args - 1):
             params[j] = tokens[j]
         params[j] = tokens[j:].join("_")
 
-        self.symbols[func_name] = Symbol(func_name, dict("type": "function", "params": params, "num_params": len(params))
+        self.symbols[func_name] = Symbol(
+            func_name, {"type": "function", "params": params, "num_params": len(params)}
+        )
 
         return [self._build_func_definition(func_name, params)]
 
@@ -119,40 +123,40 @@ class RuleEngine:
         """
         IF boolean expression THEN action .../ELSE boolean expressions THEN action
         """
-        code_rep=[]
-        tokens=tokens[1:]
-        then_i, _=self.find_next_specific(tokens, SecondaryKeywords.THEN)
-        expr_tokens=tokens[:then_i]
-        expr_str=self.parse_core(expr_tokens)
+        code_rep = []
+        tokens = tokens[1:]
+        then_i, _ = self.find_next_specific(tokens, SecondaryKeywords.THEN)
+        expr_tokens = tokens[:then_i]
+        expr_str = self.parse_core(expr_tokens)
         code_rep.append(f"if {expr_str}:\n")
-        tokens=tokens[then_i+1:]
+        tokens = tokens[then_i + 1 :]
 
-        else_i, _=self.find_next_specific(tokens, PrimaryKeywords.ELSE)
+        else_i, _ = self.find_next_specific(tokens, PrimaryKeywords.ELSE)
         if else_i == -1:
             code_rep.append("\t" + self.parse_core(tokens))
-            tokens=[]
+            tokens = []
         else:
             code_rep.append("\t" + self.parse_core(tokens[:else_i]))
-            tokens=tokens[else_i + 1:]
+            tokens = tokens[else_i + 1 :]
 
         while not self.is_EOS(tokens):
-            then_i, _=self.find_next_specific(tokens, SecondaryKeywords.THEN)
+            then_i, _ = self.find_next_specific(tokens, SecondaryKeywords.THEN)
             # Assume final else: ELSE THEN action
-            expr_tokens=tokens[:then_i]
+            expr_tokens = tokens[:then_i]
             if tokens:
-                expr_str=self.parse_core(expr_tokens)
+                expr_str = self.parse_core(expr_tokens)
                 code_rep.append(f"else {expr_str}:\n")
             else:
                 code_rep.append("else:\n")
 
-            tokens=tokens[then_i+1:]
-            else_i, _=self.find_next_specific(tokens, PrimaryKeywords.ELSE)
+            tokens = tokens[then_i + 1 :]
+            else_i, _ = self.find_next_specific(tokens, PrimaryKeywords.ELSE)
             if else_i == -1:
                 code_rep.append("\t" + self.parse_core(tokens))
-                tokens=[]
+                tokens = []
             else:
                 code_rep.append("\t" + self.parse_core(tokens[:else_i]))
-                tokens=tokens[else_i + 1:]
+                tokens = tokens[else_i + 1 :]
 
         return code_rep
 
@@ -160,13 +164,13 @@ class RuleEngine:
         """
         SET x to y
         """
-        code_rep=[]
-        to_i=self.find_next_specific(tokens, SecondaryKeywords.TO)
-        name=" ".join(tokens[1:to_i])
-        tokens=[to_i + 1:]
-        expr=self.parse_core(tokens)
+        code_rep = []
+        to_i = self.find_next_specific(tokens, SecondaryKeywords.TO)
+        name = " ".join(tokens[1:to_i])
+        tokens = RuleEngine.consume_many(tokens, to_i)
+        expr = self.parse_core(tokens)
         code_rep.append(f"{name}={expr}\n")
-        self.symbols[name]=Symbol(name, expr)
+        self.symbols[name] = Symbol(name, expr)
         return code_rep
 
     def parse_append(self, tokens):
