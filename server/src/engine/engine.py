c8d210f1821b2bb395df97a8500472f7cf9d856c
Anmol Takiar <34587426+atakiar@users.noreply.github.com>
2020-11-14 10:58:43 -0500

    chore: better merge

--- server/src/engine/engine.py
+++ server/src/engine/engine.py
@@ -1,31 +1,36 @@
 import sys
-from typing import List
+from typing import List, Optional
 
-default_code = "./src/engine/default_code.py"
+default_code_path = "./src/engine/default_code.py"
 
 
 class Engine:
-
     def __init__(self):
-        with open(default_code, "r") as fin:
-            self.code: List[str] = fin.readlines()
-            self.history = []
-            self.history_pointer = -1
+        with open(default_code_path, "r") as fin:
+            raw_code = fin.readlines()
+            self.code = self.parse_code(self.stringify_code(raw_code))
+        self.history = []
+        self.history_pointer = -1
 
-    def stringify_and_get_code(self) -> str:
-        return "".join(self.code)
+    def stringify_code(self, raw: Optional[str] = None) -> str:
+        raw = raw if raw else self.code
+        return "".join(raw)
 
-    def parse_and_set_code(self, code: str) -> None:
-        lines = code.split("\n")
+    def parse_code(self, raw: Optional[str]) -> str:
+        raw = raw if raw else self.code
+        replace_dict = {"    ": "\t", "\r\n": "\n"}
+        for k, v in replace_dict.items():
+            raw = raw.replace(k, v)
+        lines = raw.split("\n")
         code = [line + "\n" for line in lines]
-        self.code = code
+        return code
 
-    def _save_history(self, code):
-        self.history = self.history[:self.history_pointer + 1]
+    def save_history(self, code):
+        self.history = self.history[: self.history_pointer + 1]
         self.history.append(code)
         self.history_pointer = len(self.history) - 1
 
-    def _undo(self, code):
+    def undo(self, code):
         if self.history_pointer > 0:
             self.history_pointer -= 1
         return self.history[self.history_pointer]
