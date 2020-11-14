aac5754e08b775a2648497b16db26c313375eca5
jguo1249 <j.guo1249@gmail.com>
2020-11-14 14:27:58 -0500

    function parser, symbol class

--- server/src/rule_engine/rule_engine.py
+++ server/src/rule_engine/rule_engine.py
@@ -1,11 +1,29 @@
 import sys
+from typing import List
 from .keywords import PrimaryKeywords, SecondaryKeywords
+
+
+class Symbol:
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
 
 
 class RuleEngine:
     def __init__(self):
         # TODO: Dict of something
         self.symbols = {}
+        self.nums = {"one": 1,
+                     "two": 2,
+                     "three": 3,
+                     "four": 4,
+                     "five": 5,
+                     "six": 6,
+                     "seven": 7,
+                     "eight": 8,
+                     "nine": 9,
+                     "ten": 10
+                     }
 
     def parse(self, text: str):
         tokens = text.strip().split(" ")
@@ -29,8 +47,31 @@ class RuleEngine:
         else:
             print("NOT IMPLEMENTED EXCEPTION")
 
-    def parse_function(self, tokens):
-        pass
+    def _build_func_definition(name: str, params: List[str]) -> str:
+        paramstr = ", ".join(params)
+        return f"def {name}({paramstr}):\n"
+
+    def parse_function(self, tokens: List[str]):
+        arg_i = -1
+        and_i = -1  # Assume only 1 "and" per definition
+        for i, t in enumerate(tokens):
+            if t == "argument" or t == "arguments":
+                arg_i = i
+            if t == "and":
+                and_i = i
+
+        num_args = self.nums[tokens[arg_i-1]]
+        params = ["" for i in range(num_args)]
+        func_name = "_".join(tokens[1:arg_i-2])
+        if and_i != -1:
+            params[-1] = " ".join(tokens[and_i:])
+            tokens = tokens[:and_i]
+            num_args = num_args - 1
+        tokens = tokens[arg_i + 1:]
+        for j in range(num_args-1):
+            params[j] = tokens[j]
+        params[j] = tokens[j:].join("_")
+        return _build_func_definition(func_name, params)
 
     def parse_call(self, tokens):
         pass
