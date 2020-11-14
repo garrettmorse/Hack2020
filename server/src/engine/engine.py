fbe7d1c3980c652ccdbdf069f0dadd07bed3fb76
Anmol Takiar <34587426+atakiar@users.noreply.github.com>
2020-11-14 10:42:57 -0500

    feat: update engine

--- server/src/engine/engine.py
+++ server/src/engine/engine.py
@@ -1,18 +1,24 @@
 import sys
-from typing import List
+from typing import List, Optional
 
-default_code = "./src/engine/default_code.py"
+default_code_path = "./src/engine/default_code.py"
 
 
 class Engine:
     def __init__(self):
-        with open(default_code, "r") as fin:
-            self.code: List[str] = fin.readlines()
+        with open(default_code_path, "r") as fin:
+            raw_code = fin.readlines()
+            self.code = self.parse_code(self.stringify_code(raw_code))
 
-    def stringify_and_get_code(self) -> str:
-        return "".join(self.code)
+    def stringify_code(self, raw: Optional[str] = None) -> str:
+        raw = raw if raw else self.code
+        return "".join(raw)
 
-    def parse_and_set_code(self, code: str) -> None:
-        lines = code.split("\n")
+    def parse_code(self, raw: Optional[str]) -> str:
+        raw = raw if raw else self.code
+        replace_dict = {"    ": "\t", "\r\n": "\n"}
+        for k, v in replace_dict.items():
+            raw = raw.replace(k, v)
+        lines = raw.split("\n")
         code = [line + "\n" for line in lines]
-        self.code = code
+        return code
