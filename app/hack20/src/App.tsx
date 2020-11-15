4ae84a7ca7cb9ed034280087ff8fdfb712d8bfd6
Garrett Morse <morse.172@osu.edu>
2020-11-14 22:00:14 -0500

    more styling changes boi

--- app/hack20/src/App.tsx
+++ app/hack20/src/App.tsx
@@ -7,7 +7,7 @@ import Output from './components/Output';
 import AppRoutes from './service/app-routes';
 import { sample } from './static/constants.json';
 
-class App extends React.Component<{}, { execLoader: boolean, sendLoader: boolean, transcript: string, code: string, output: string }> {
+class App extends React.Component<{}, { codeEdited: boolean, execLoader: boolean, sendLoader: boolean, transcript: string, code: string, output: string }> {
   constructor(props: any) {
     super(props);
     this.state = {
@@ -15,16 +15,17 @@ class App extends React.Component<{}, { execLoader: boolean, sendLoader: boolean
       sendLoader: false,
       transcript: '',
       code: sample,
+      codeEdited: false,
       output: ''
     }
   }
   sendRecording = (finalScript: string) => {
-    const data = JSON.stringify({ transcript: finalScript, code: this.state.code });
-    console.log(`recording being sent: ${data}`);
+    const data = JSON.stringify({ transcript: finalScript, code: this.state.code, edited: this.state.codeEdited });
+    console.log(data);
     this.setState({ sendLoader: true });
     AppRoutes.sendText(data)
       .then(res => {
-        this.onCodeChange(res.data);
+        this.onCodeChange(res.data.code, false);
         this.setState({ sendLoader: false });
       })
       .catch(e => {
@@ -35,13 +36,14 @@ class App extends React.Component<{}, { execLoader: boolean, sendLoader: boolean
 
   execCode = () => {
     const data = JSON.stringify({ code: this.state.code });
-    console.log(`code being sent: ${data}`);
+    console.log(data);
     this.setState({ execLoader: true });
     AppRoutes.execCode(data)
       .then(async res => {
         // await this.sleep(2000);
         console.log(res);
         this.onOutputChange(res.data.output);
+        this.onCodeChange(res.data.code, false);
         this.setState({ execLoader: false });
       })
       .catch(e => {
@@ -55,16 +57,26 @@ class App extends React.Component<{}, { execLoader: boolean, sendLoader: boolean
     // console.log(`update state.transcript: ${ this.state.transcript }`);
   }
 
-  onCodeChange = (newCode: string) => {
-    this.setState({ code: newCode });
+  onCodeChange = (newCode: string, codeEdited = true) => {
+    this.setState({ code: newCode, codeEdited });
     console.log(`update state.code: ${this.state.code}`);
   }
 
+  undoCode = async () => {
+    await AppRoutes.undoCode()
+      .then(res => {
+        this.onCodeChange(res.data.code);
+      });
+  }
+
   onOutputChange = (newOutput: string) => {
-    this.setState({ output: newOutput });
+    this.setState({ output: this.state.output + '\n-----\n' + newOutput });
     console.log(`update state.output: ${this.state.output}`);
   }
 
+  onOutputClear = () => {
+    this.setState({ output: '' });
+  }
   sleep(ms: any) {
     return new Promise(resolve => setTimeout(resolve, ms));
   }
@@ -81,28 +93,38 @@ class App extends React.Component<{}, { execLoader: boolean, sendLoader: boolean
                 <Header as='h3'>Code</Header>
                 <MyEditor loading={this.state.execLoader}
                   handleCode={this.execCode.bind(this)}
-                  onCodeChange={this.onCodeChange.bind(this)}
+                  onCodeChange={(newCode: string) => this.onCodeChange(newCode)}
+                  handleUndo={this.undoCode}
                   code={this.state.code} />
               </Grid.Column>
               <Grid.Column>
-                <Container style={{ height: '40%' }}>
+
+                <Container style={{ height: '50%' }}>
+                  <Listen executeCode={this.execCode}
+                    loading={this.state.sendLoader}
+                    onTextChange={this.onTextChange}
+                    onCodeChange={(newCode: string) => { this.onCodeChange(newCode, false) }}
+                    text={this.state.transcript}
+                    handleUndo={this.undoCode}
+                    handleRecording={this.sendRecording}
+                    handleOutputClear={this.onOutputClear}
+                  />
+                </Container>
+                <Divider horizontal />
+                <Container >
                   <Header as='h3'>Output</Header>
                   <span style={{ whiteSpace: 'pre-line' }}>{this.state.output}</span>
                   {/* <Output output={this.state.output} /> */}
                 </Container>
-                <Divider horizontal />
-                <Container>
-                  <Listen executeCode={this.execCode}
-                    loading={this.state.sendLoader}
-                    onTextChange={this.onTextChange}
-                    onCodeChange={this.onCodeChange}
-                    text={this.state.transcript}
-                    handleRecording={this.sendRecording.bind(this)} />
-                </Container>
+
               </Grid.Column>
             </Grid>
 
-            <Divider vertical><Icon name='long arrow alternate right' /></Divider>
+            <Divider hidden style={{ top: '67%' }} vertical>
+              <Icon name='long arrow alternate right' />
+
+            </Divider>
+            <Divider hidden style={{ top: '33%' }} vertical><Icon name='long arrow alternate left' /></Divider>
 
           </Segment>
         </div>
